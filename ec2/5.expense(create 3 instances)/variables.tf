e2812fa9babc9dc5d6a458b9ebab16e2a3938d88
Chinnamachireddy <chinnamachireddy222@gmail.com>
2024-08-12 06:56:23 +0530

    locals

---
+++ ec2/5.expense(create 3 instances)/variables.tf
@@ -0,0 +1,55 @@
+#ec2 variables
+variable "instance_names" {
+    type = list
+    default = ["db", "backend", "frontend" ]
+}
+
+variable "image_id" {
+  type    = string
+  default = "ami-041e2ea9402c46c32" #Optional
+  description = "RHEL 9 AMI ID" #optional
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "common_tags" {
+    default = {
+        project = "Expense"
+        Environment = "Dev"
+        Teffaform = "true"
+
+    }
+}
+
+# sg variables
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+variable "ssh_port" {
+  default = 22
+}
+
+variable "protocol" {
+  default = "tcp"
+}
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+#r53 variables
+variable "zone_id" {
+    default = "Z08422883FRLSZ1ROER1K"
+}
+
+variable "domain_name" {
+    default = "chinna.online"
+}
